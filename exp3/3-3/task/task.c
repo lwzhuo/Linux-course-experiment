a37bb1d3d8d3daf6c3f5b2b4d3c79707243e79b9
l10062997 <lwzhuo@live.com>
2018-06-01 14:10:12 +0800

    change color

--- exp3/3-3/task/task.c
+++ exp3/3-3/task/task.c
@@ -24,17 +24,17 @@ void *sender(void *arg){
         if((msgrcv(msqid,&mymsg,MSG_SIZE,1,IPC_NOWAIT))>0)
             puts(mymsg.mtext);
         if(flag){
-            printf("Sender exit!\n");
+            printf("\033[31m\033[1mSender exit!\033[0m\n");
             return;
         }
-        printf("Sender input:");
+        printf("\033[33m\033[1mSender input:\033[0m");
         gets(buff);
         if(!strcmp(buff,"exit"))
             flag=1;
         mymsg.mtype=1;
         strcpy(mymsg.mtext,buff);
         if(msgsnd(msqid,&mymsg,MSG_SIZE,0)<0)
-            printf("Sender:msgsend error\n");
+            printf("\033[31m\033[1mSender:msgsend error!\033[0m\n");
         sem_post(&mutex);
         sem_post(&full);
     }
@@ -46,13 +46,13 @@ void *receiver(void *arg){
         sem_wait(&full);
         sem_wait(&mutex);
         if((msgrcv(msqid,&mymsg,MSG_SIZE,1,0))<0)
-            printf("Receiver:msgsend error\n");
+            printf("\033[31m\033[1mReceiver:msgsend error!\033[0m\n");
         else{
             if(strcmp(mymsg.mtext,"exit"))
-                snprintf(buff,512,"Receiver reply \"%s\":over",mymsg.mtext);
+                snprintf(buff,512,"\033[36m\033[01mReceiver reply \"%s\"\033[0m:over",mymsg.mtext);
             else{
                 flag = 1;
-                snprintf(buff,512,"Receiver reply \"%s\":over\nReceiver exit!",mymsg.mtext);
+                snprintf(buff,512,"\033[36m\033[01mReceiver reply \"%s\"\033[0m:over\n\033[31m\033[01mReceiver exit!\033[0m",mymsg.mtext);
             }
             strcpy(mymsg.mtext,buff);
             mymsg.mtype=1;
@@ -72,7 +72,8 @@ int main()
     sem_init(&full,0,0);
     sem_init(&empty,0,1);
 //创建消息队列
-    if((msqid=msgget(IPC_PRIVATE,IPC_CREAT|0660))<0)//很坑这里，除了表示创建的flag，读写权限也要加上
+    //这里很坑，除了表示创建的flag，读写权限也要加上
+    if((msqid=msgget(IPC_PRIVATE,IPC_CREAT|0660))<0)
         printf("megget error\n");
 //创建线程
     pthread_create(&Sender,NULL,sender,NULL);//创建sender线程，执行sender函数,不传入参数，不对线程属性进行定制
